c8c18bcc9ba92736e2d72a32c491c14d92fbc553
de-wan <stephen.wandie@churpy.co>
2024-11-29 17:18:48 +0300

    update: app database

--- composeApp/src/commonMain/kotlin/co/ke/foxlysoft/budgetgain/database/AppDatabase.kt
+++ composeApp/src/commonMain/kotlin/co/ke/foxlysoft/budgetgain/database/AppDatabase.kt
@@ -4,11 +4,12 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 //import androidx.room.RoomDatabaseConstructor
 
-@Database(entities = [UserEntity::class, SettingsEntity::class, BudgetEntity::class], version = 2)
+@Database(entities = [UserEntity::class, SettingsEntity::class, BudgetEntity::class, CategoryEntity::class], version = 3)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun settingsDao(): SettingsDao
     abstract fun userDao(): UserDao
     abstract fun budgetDao(): BudgetDao
+    abstract fun categoryDao(): CategoryDao
 }
 
 //@Suppress("NO_ACTUAL_FOR_EXPECT")
