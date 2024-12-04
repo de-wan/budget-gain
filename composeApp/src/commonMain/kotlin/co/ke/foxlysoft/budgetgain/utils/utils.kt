a1b0c656213d2b75e35da7d03e5cc7db2fe19dc5
de-wan <stephen.wandie@churpy.co>
2024-12-04 17:54:10 +0300

    utils: added cents operations utils: added date millis operations

--- composeApp/src/commonMain/kotlin/co/ke/foxlysoft/budgetgain/utils/utils.kt
+++ composeApp/src/commonMain/kotlin/co/ke/foxlysoft/budgetgain/utils/utils.kt
@@ -1,6 +1,9 @@
 package co.ke.foxlysoft.budgetgain.utils
 
 import androidx.compose.ui.text.input.TextFieldValue
+import kotlinx.datetime.Instant
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
 
 
 data class TextFieldInput(
@@ -17,3 +20,35 @@ data class ErrorStatus(
     val isError: Boolean,
     val errorMsg: String? = null,
 )
+
+fun dateMillisToString(millis: Long): String {
+    return Instant.fromEpochMilliseconds(epochMilliseconds = millis).toLocalDateTime(TimeZone.currentSystemDefault()).date.toString()
+}
+
+fun amountToCents(amount: String): Long {
+    // Split the string into whole and fractional parts
+    val parts = amount.split(".")
+    val wholePart = parts[0] // Before the decimal point
+    val fractionalPart = if (parts.size > 1) parts[1] else "0" // After the decimal point
+
+    // Ensure the fractional part has exactly two digits
+    val adjustedFractionalPart = fractionalPart.padEnd(2, '0').take(2)
+
+    // Combine whole and fractional parts into cents
+    return (wholePart + adjustedFractionalPart).toLong()
+}
+
+fun centsToString(cents: Long): String {
+    // Separate whole dollars and fractional cents
+    val dollars = cents / 100
+    val fractionalCents = cents % 100
+
+    // Format using string templates and pad fractional part to two digits
+    return "$dollars.${fractionalCents.toString().padStart(2, '0')}"
+}
+
+fun isValidAmount(amount: String): Boolean {
+    // Check if the amount is not empty and matches the regex pattern for valid amounts
+    val regex = "^\\d+(\\.\\d{1,2})?$".toRegex()
+    return amount.matches(regex)
+}
