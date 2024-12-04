935eeb7ae57bb035cfb926784b6f9cdfcc992edd
de-wan <stephen.wandie@churpy.co>
2024-12-04 18:06:34 +0300

    chore(createCategory): registered category screen dependencies in koin

--- composeApp/src/commonMain/kotlin/co/ke/foxlysoft/budgetgain/di/KoinModule.kt
+++ composeApp/src/commonMain/kotlin/co/ke/foxlysoft/budgetgain/di/KoinModule.kt
@@ -7,12 +7,15 @@ import org.koin.dsl.module
 
 import co.ke.foxlysoft.budgetgain.repos.SettingsRepository
 import co.ke.foxlysoft.budgetgain.repos.BudgetRepository
+import co.ke.foxlysoft.budgetgain.repos.CategoryRepository
 import org.koin.compose.viewmodel.dsl.viewModelOf
 import org.koin.core.context.startKoin
 
 import co.ke.foxlysoft.budgetgain.ui.HomeScreenViewModel
 import co.ke.foxlysoft.budgetgain.ui.AllBudgetsScreenViewModel
 import co.ke.foxlysoft.budgetgain.ui.CreateBudgetScreenViewModel
+import co.ke.foxlysoft.budgetgain.ui.AddCategoryScreenViewModel
+
 
 expect fun platformModule(): Module
 
@@ -37,9 +40,15 @@ val provideBudgetRepository =
         singleOf(::BudgetRepository)
     }
 
+val provideCategoryRepository =
+    module {
+        singleOf(::CategoryRepository)
+    }
+
 val viewModelModule =
     module {
         viewModelOf(::HomeScreenViewModel)
         viewModelOf(::AllBudgetsScreenViewModel)
         viewModelOf(::CreateBudgetScreenViewModel)
+        viewModelOf(::AddCategoryScreenViewModel)
     }
