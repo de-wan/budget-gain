e12cdd0f4f2146528e0991224542ec610ef244b7
de-wan <stephen.wandie@churpy.co>
2024-12-13 13:18:40 +0300

    chore: added tests for sms parser

--- composeApp/src/commonMain/kotlin/co/ke/foxlysoft/budgetgain/utils/MpesaSmsTypes.kt
+++ composeApp/src/commonMain/kotlin/co/ke/foxlysoft/budgetgain/utils/MpesaSmsTypes.kt
@@ -1,12 +1,12 @@
 package co.ke.foxlysoft.budgetgain.utils
 
 enum class MpesaSmsTypes {
-    SEND_MONEY,
-    RECEIVE_MONEY,
-    WITHDRAW_MONEY,
+    SEND_MONEY,     // has phone number before on
+    POCHI,          // has no phone number before on
+    RECEIVE_MONEY,  // has received
+    WITHDRAW_MONEY, // has Withdraw
     DEPOSIT_MONEY, // TODO: Need a sample sms to test this
-    POCHI,
-    PAYBILL,
-    TILL,
+    PAYBILL,        // has for account
+    TILL,           // has paid to
     UNKNOWN
 }
--- composeApp/src/commonTest/kotlin/co/ke/foxlysoft/budgetgain/utils/SmsParseTest.kt
+++ composeApp/src/commonTest/kotlin/co/ke/foxlysoft/budgetgain/utils/SmsParseTest.kt
@@ -18,8 +18,8 @@ class SmsParseTest {
         assertEquals(t.subjectPrimaryIdentifier, "0723456789")
         assertEquals(t.date, "8/12/24")
         assertEquals(t.time, "1:53 PM")
-        assertEquals(t.balance, 10000)
-        assertEquals(t.cost, 23)
+        assertEquals(t.balance, 1000000)
+        assertEquals(t.cost, 2300)
     }
 
     @Test
@@ -28,6 +28,14 @@ class SmsParseTest {
         val t = extractReceiveSms(sms)
 
         assertEquals(t.smsType, MpesaSmsTypes.RECEIVE_MONEY)
+        assertEquals(t.subjectPrimaryIdentifierType, "phone")
+        assertEquals(t.subjectPrimaryIdentifier, "0728374654")
+        assertEquals(t.subjectSecondaryIdentifierType, "name")
+        assertEquals(t.subjectSecondaryIdentifier, "John Doe")
+        assertEquals(t.ref, "SL0348234Z")
+        assertEquals(t.amount, 2300000)
+        assertEquals(t.balance, 52300000)
+        assertEquals(t.cost, 0)
     }
 
     @Test
@@ -51,5 +59,58 @@ class SmsParseTest {
         val t1 = extractPochiSms(sendSms)
 
         assertEquals(MpesaSmsTypes.UNKNOWN, t1.smsType)
+    }
+
+    @Test
+    fun testExtractTillSms() {
+        val tillSms = "SYEISFNEFS Confirmed. Ksh1,500.00 paid to FOXLYSOFT TECHNOLOGIES - KENYA. on 11/11/24 at 9:25 PM.New M-PESA balance is Ksh1,000,000.00. Transaction cost, Ksh0.00."
+        val t = extractTillSms(tillSms)
+
+        assertEquals("SYEISFNEFS", t.ref)
+        assertEquals(150000, t.amount)
+        assertEquals("FOXLYSOFT TECHNOLOGIES - KENYA", t.subjectPrimaryIdentifier)
+        assertEquals("name", t.subjectPrimaryIdentifierType)
+        assertEquals("11/11/24", t.date)
+        assertEquals("9:25 PM", t.time)
+        assertEquals(100000000, t.balance)
+        assertEquals(0, t.cost)
+        assertEquals(MpesaSmsTypes.TILL, t.smsType)
+    }
+
+    @Test
+    fun testExtractPaybillSms() {
+        val paybillSms = "SYEISFNEFS Confirmed. Ksh800.00 sent to PayPal Top Up Account for account 0729511560 on 11/11/24 at 9:25 PM New M-PESA balance is Ksh1,000,000.00. Transaction cost, Ksh0.00."
+        val t = extractPaybillSms(paybillSms)
+
+        assertEquals("SYEISFNEFS", t.ref)
+        assertEquals(80000, t.amount)
+        assertEquals("paybill", t.subjectPrimaryIdentifierType)
+        assertEquals("PayPal Top Up Account", t.subjectPrimaryIdentifier)
+        assertEquals("account", t.subjectSecondaryIdentifierType)
+        assertEquals("0729511560", t.subjectSecondaryIdentifier)
+        assertEquals("11/11/24", t.date)
+        assertEquals("9:25 PM", t.time)
+        assertEquals(100000000, t.balance)
+        assertEquals(0, t.cost)
+        assertEquals(MpesaSmsTypes.PAYBILL, t.smsType)
+    }
+
+    @Test
+    fun testExtractWithdrawSms() {
+        val tillSms = "SL85OT9QWF Confirmed.on 8/12/24 at 1:53 PMWithdraw Ksh1,500.00 from 3234323 - FOXLYSOFT TECHNOLOGIES KENYA New M-PESA balance is Ksh10,000.00. Transaction cost, Ksh23.00."
+        val t = extractWithdrawSms(tillSms)
+
+        assertEquals("SL85OT9QWF", t.ref)
+        assertEquals("8/12/24", t.date)
+        assertEquals("1:53 PM", t.time)
+        assertEquals(150000, t.amount)
+        assertEquals("till", t.subjectPrimaryIdentifierType)
+        assertEquals("3234323", t.subjectPrimaryIdentifier)
+        assertEquals("name", t.subjectSecondaryIdentifierType)
+        assertEquals("FOXLYSOFT TECHNOLOGIES KENYA", t.subjectSecondaryIdentifier)
+        assertEquals(1000000, t.balance)
+        assertEquals(2300, t.cost)
+
+        assertEquals(MpesaSmsTypes.WITHDRAW_MONEY, t.smsType)
     }
 }
