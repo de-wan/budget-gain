aeb5bf7993466cf88d804bbde1dbe9eab0759eca
de-wan <stephen.wandie@churpy.co>
2025-02-14 16:37:18 +0300

    feat: merchant transactions scroll

--- composeApp/src/commonMain/kotlin/co/ke/foxlysoft/budgetgain/ui/MerchantsScreenViewModel.kt
+++ composeApp/src/commonMain/kotlin/co/ke/foxlysoft/budgetgain/ui/MerchantsScreenViewModel.kt
@@ -1,25 +1,18 @@
 package co.ke.foxlysoft.budgetgain.ui
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import co.ke.foxlysoft.budgetgain.database.AccountEntity
-import co.ke.foxlysoft.budgetgain.database.SettingsEntity
 import co.ke.foxlysoft.budgetgain.repos.AccountRepository
 import co.ke.foxlysoft.budgetgain.repos.BudgetRepository
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.stateIn
 
 class MerchantsScreenViewModel(
     private val accountRepository: AccountRepository,
     private val budgetRepository: BudgetRepository,
 ): ViewModel() {
-    val merchantAccounts: StateFlow<List<AccountEntity>> = accountRepository.getMerchantAccounts()
-        .stateIn(
-            scope = viewModelScope,
-            started = SharingStarted.WhileSubscribed(5000),
-            initialValue = emptyList()
-        )
+
+    suspend fun getMerchantAccounts(limit: Int, offset: Int): List<AccountEntity> {
+        return accountRepository.getPagingMerchantAccounts(limit, offset)
+    }
 
     suspend fun getMerchantAccountBudgetSpend(merchantAccountId: Long): Long {
         val budget = budgetRepository.getCurrentBudget()
