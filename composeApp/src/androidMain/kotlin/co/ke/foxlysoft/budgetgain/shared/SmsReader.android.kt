166f4cc6f66b7464f8096a0c019e242918ab78a7
de-wan <steve.wandie90@gmail.com>
2026-02-08 11:59:13 +0300

    feat: Added search to sms inbox

--- composeApp/src/androidMain/kotlin/co/ke/foxlysoft/budgetgain/shared/SmsReader.android.kt
+++ composeApp/src/androidMain/kotlin/co/ke/foxlysoft/budgetgain/shared/SmsReader.android.kt
@@ -5,16 +5,17 @@ import co.ke.foxlysoft.budgetgain.ContextProvider
 import co.ke.foxlysoft.budgetgain.utils.dateMillisToString
 
 actual class SmsReader {
-    actual fun getMpesaSms(fromDate: Long, toDate: Long): List<String> {
+    actual fun getMpesaSms(fromDate: Long, toDate: Long, searchQuery: String?): List<String> {
         val context = ContextProvider.context;
 
         println("From date: ${dateMillisToString(fromDate)}, To date: ${dateMillisToString(toDate)}")
+        val search = searchQuery ?: ""
 
         val smsList = mutableListOf<String>()
         val uri = Uri.parse("content://sms/inbox")
         val projection = arrayOf("_id", "address", "body", "date")
-        val selection = "address = ? AND date >= ? AND date <= ?"
-        val selectionArgs = arrayOf("MPESA", fromDate.toString(), toDate.toString())
+        val selection = "address = ? AND date >= ? AND date <= ? AND body LIKE ?"
+        val selectionArgs = arrayOf("MPESA", fromDate.toString(), toDate.toString(), "%$search%")
 
         val cursor = context.contentResolver.query(
             uri, projection, selection, selectionArgs, "date"
